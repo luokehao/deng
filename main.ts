e30553391e05b8c4ca6b221d21db32ea469791c2
骆柯豪 <141896132+luokehao@users.noreply.github.com>
2023-10-14 12:54:27 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+})
+basic.forever(function () {
+	
+})
